587bcf143e3d238def9c0da0506ce0848a51a1f2
WillianJohan <johan_willian@hotmail.com>
2019-09-17 21:29:59 -0300

    Code for Read and Write Files

---
+++ C#/PesquisaOrdenacao/FileManager.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Rename all Sort word to Sorter
+
+namespace PesquisaOrdenacao
+{
+	public class FileManager
+	{	
+		private string fileName = "";
+		private List<Statistic> statistics = new List<Statistic>();
+		//public
+	}
+}
