9e2f4c338a968727bde668acf41f30b85d54607f
WillianJohan <johan_willian@hotmail.com>
2019-09-17 21:34:36 -0300

    New Changes - Incomplete

--- C#/PesquisaOrdenacao/MainWindow.cs
+++ C#/PesquisaOrdenacao/MainWindow.cs
@@ -42,45 +42,31 @@ namespace PesquisaOrdenacao
 
         private void ButtonStart_Click(object sender, EventArgs e)
         {
+
+
             int gerados = (int)totGerados.Value;
             SortMethods.SorterSetup.setup(gerados);
             //comaca aqui
 
             lbl_StatusBubble.Text = "Em Execucao";
             SortMethods.CombSort combSort = new SortMethods.CombSort();
-            combSort.setupList();
-            sw = Stopwatch.StartNew();
             combSort.sort();
-            Console.WriteLine(sw.ElapsedMilliseconds);
-            sw.Stop();
             lbl_StatusBubble.Text = "Finalizado";
 
 
             lbl_StatusInsertion.Text = "Em Execucao";
             SortMethods.InsertionSort insertionSort = new SortMethods.InsertionSort();
-            insertionSort.setupList();            
-            sw = Stopwatch.StartNew();
             insertionSort.sort();
-            Console.WriteLine(sw.ElapsedMilliseconds);
-            sw.Stop();
             lbl_StatusInsertion.Text = "Finalizado";
             
             lbl_StatusSelection.Text = "Em Execucao";
             SortMethods.SelectionSort selectionSort = new SortMethods.SelectionSort();
-            selectionSort.setupList();
-            sw = Stopwatch.StartNew();
             selectionSort.sort();
-            Console.WriteLine(sw.ElapsedMilliseconds);
-            sw.Stop();
             lbl_StatusSelection.Text = "Finalizado";
 
             lbl_StatusBubble.Text = "Em Execucao";
             SortMethods.BubbleSort bubbleSort = new SortMethods.BubbleSort();
-            bubbleSort.setupList();
-            sw = Stopwatch.StartNew();
             bubbleSort.sort();
-            Console.WriteLine(sw.ElapsedMilliseconds);
-            sw.Stop();
             lbl_StatusBubble.Text = "Finalizado";
 
 
