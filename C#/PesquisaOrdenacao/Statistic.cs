dc0d6e12f401506b8da933bbf981de3dc825628c
WillianJohan <johan_willian@hotmail.com>
2019-09-17 21:31:07 -0300

    Created for get statistic of a sorter method

---
+++ C#/PesquisaOrdenacao/Statistic.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesquisaOrdenacao
+{
+	public class Statistic
+	{	
+		private string methodName;
+		private string time;
+		private string comparations;
+		private string exchanges;
+
+		public Statistic(string methodName, string time, string comparations, string exchanges){
+			this.methodName = methodName;
+			this.time = time;
+			this.comparations = comparations;
+			this.exchanges = exchanges;
+		}
+
+		public string getStatistic(){
+			return methodName+"@"+time+"@"+comparations+"@"+exchanges;
+		}
+	}
+}
