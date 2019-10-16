60ee889e6e25c6683acc6e72c5ddcf359dfab8a7
Laboratório 25 <usrlab25@lanfran.local>
2019-10-16 09:21:34 -0300

    Metodo de pesquisa adicionado

---
+++ C#/Model/SearchMethods/MySearchMethod.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesquisaOrdenacao.Model.SearchMethods
+{
+    class MySearchMethod
+    {
+        /*
+         * Algorítimo deve ler arquivo e procurar por palavras
+         * Ler um lbl, texto, String
+         * 
+         * 
+         */
+
+        private string text = null;
+        private string word = null;
+
+        public string Text { get => text; set => text = value; }
+        public string Word { get => word; set => word = value; }
+
+        public MySearchMethod(string _text)
+        {
+            Text = _text;
+        }
+
+        public MySearchMethod(){}
+
+        public Boolean canIFoundThisInText(string _word)
+        {
+
+            return false;
+        }
+
+        public Boolean canIFoundThisInText(string _word, string _text)
+        {
+
+            return false;
+        }
+    }
+}
