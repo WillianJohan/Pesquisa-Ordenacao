3234ae7412d342801e72c8073840f9bad7514dc5
Willian Johan <johan_willian@hotmail.com>
2019-10-21 16:49:21 -0300

    Pasta renomeada

---
+++ CodigoFonte/Model/SortMethods/Selection.cs
@@ -0,0 +1,36 @@
+namespace PesquisaOrdenacao.Model.SortMethods
+{
+    public class Selection : Sort
+    {
+        public Selection()
+        {
+            MethodName = "SelectionSort";
+        }
+
+        protected override void StartSorter()
+        {
+            int smallest_Index = 0;
+            int i, j;
+
+            for (i = 0; i < vetor.Count; i++)
+            {
+                smallest_Index = i;
+                for (j = i + 1; j < vetor.Count; j++)
+                {
+                    comparisons++;
+                    if (vetor[j] < vetor[smallest_Index])
+                    {
+                        smallest_Index = j;
+                    }
+                }
+                if (vetor[smallest_Index] != vetor[i])
+                {
+                    exchanges++;
+                    int aux = vetor[smallest_Index];
+                    vetor[smallest_Index] = vetor[i];
+                    vetor[i] = aux;
+                }
+            }
+        }
+    }
+}
