84f4314b917caf687ad5269e482a0e2facf20ecf
willianjohan <johan_willian@hotmail.com>
2019-10-24 11:30:51 -0300

    Pesquisa com Windwos Forms

---
+++ TrabalhoPesquisa/TrabalhoPesquisa/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace TrabalhoPesquisa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.caixaDeTexto = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.caixaDePesquisa = new System.Windows.Forms.TextBox();
+            this.lbl_Resultados = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // caixaDeTexto
+            // 
+            this.caixaDeTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.caixaDeTexto.Location = new System.Drawing.Point(12, 148);
+            this.caixaDeTexto.Name = "caixaDeTexto";
+            this.caixaDeTexto.Size = new System.Drawing.Size(912, 549);
+            this.caixaDeTexto.TabIndex = 0;
+            this.caixaDeTexto.Text = "";
+            this.caixaDeTexto.TextChanged += new System.EventHandler(this.CaixaDeTexto_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(242, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(499, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Programa exemplo de Pesquisa";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1304, 72);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(634, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pesquisa:";
+            // 
+            // caixaDePesquisa
+            // 
+            this.caixaDePesquisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.caixaDePesquisa.Location = new System.Drawing.Point(717, 102);
+            this.caixaDePesquisa.Name = "caixaDePesquisa";
+            this.caixaDePesquisa.Size = new System.Drawing.Size(207, 26);
+            this.caixaDePesquisa.TabIndex = 4;
+            this.caixaDePesquisa.TextChanged += new System.EventHandler(this.CaixaDePesquisa_TextChanged);
+            // 
+            // lbl_Resultados
+            // 
+            this.lbl_Resultados.AutoSize = true;
+            this.lbl_Resultados.Location = new System.Drawing.Point(714, 131);
+            this.lbl_Resultados.Name = "lbl_Resultados";
+            this.lbl_Resultados.Size = new System.Drawing.Size(84, 13);
+            this.lbl_Resultados.TabIndex = 5;
+            this.lbl_Resultados.Text = "Resultados: 289";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(936, 709);
+            this.Controls.Add(this.lbl_Resultados);
+            this.Controls.Add(this.caixaDePesquisa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.caixaDeTexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox caixaDeTexto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox caixaDePesquisa;
+        private System.Windows.Forms.Label lbl_Resultados;
+    }
+}
+
