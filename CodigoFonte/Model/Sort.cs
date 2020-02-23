c278ac9ec0ca4a376e36118010d4845972e455cd
willianjohan <johan_willian@hotmail.com>
2020-02-23 15:04:54 -0300

    Adicionado comentarios em Model.Sort

--- CodigoFonte/Model/Sort.cs
+++ CodigoFonte/Model/Sort.cs
@@ -8,12 +8,12 @@ namespace PesquisaOrdenacao.Model
     {
         //Read all the numbers and add to the list
         private string methodName = "";
-        protected List<int> vetor = new List<int>();
+        protected List<int> listOfUnsortedNumbers = new List<int>();
         protected Int64 comparisons = 0;
         protected Int64 exchanges = 0;
-        protected Stopwatch sw; //Calculate the time
-        protected Statistic statistic;
-        private bool isFinished = false; //Return true if the sorter is finished
+        protected Stopwatch stopwatch; //Stores the execution time of the ordering method
+        protected Statistic statistic; //Stores all the data of time, exchanges and comparations.
+        private bool isFinished = false; //Return true if the sorter is finished.
 
         //Returns the name of sort method. 
         public string MethodName { get => methodName; set => methodName = value; }
@@ -22,22 +22,22 @@ namespace PesquisaOrdenacao.Model
         public void setupList()
         {
             //Read the archive and add values in the list
-            vetor = Control.FileManager.ReadUnsortedNumbers();
+            listOfUnsortedNumbers = Control.FileManager.ReadUnsortedNumbers();
         }
 
         public void sort()
         {
             setupList();
-            sw = Stopwatch.StartNew();
+            stopwatch = Stopwatch.StartNew();
             StartSorter();
-            sw.Stop();
-            statistic = new Statistic(MethodName, sw.Elapsed.ToString(), comparisons.ToString(), exchanges.ToString());
-            isFinished = true;
+            stopwatch.Stop();
+            statistic = new Statistic(MethodName, stopwatch.Elapsed.ToString(), comparisons.ToString(), exchanges.ToString());
+            IsFinished = true;
         }
 
         public void Record()
         {
-            Control.FileManager.RecordUnsortedNumbers(vetor, MethodName);
+            Control.FileManager.RecordUnsortedNumbers(listOfUnsortedNumbers, MethodName);
         }
 
         public Statistic getStatistics()
