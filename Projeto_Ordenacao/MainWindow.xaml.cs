0ed5adf99dbb0e8d26334e20c0d6700abdd12cc9
WillianJohan <johan_willian@hotmail.com>
2020-03-02 21:14:42 -0300

    BackgroundWorker implementado

--- Projeto_Ordenacao/MainWindow.xaml.cs
+++ Projeto_Ordenacao/MainWindow.xaml.cs
@@ -5,19 +5,27 @@ using System.Collections.Generic;
 using Ordenacao.Control;
 using Ordenacao.Model;
 using Ordenacao.Model.SortMethods;
-
+using System.ComponentModel;
 
 namespace Projeto_Ordenacao_WPF
 {
 
     public partial class MainWindow : Window
     {
-        private int numerosParaGerar = 0;
-        private List<Statistic> listOfStatistics;
+        private int amountOfNumbersToGenerate = 0;
+        private int currentIndexMethod = 0;
+        private int totalMethods = 8;
+        private string currentMethodName;
+        private List<ISort> sorterMethods;
+        private List<Statistic> listOfStatistics;        
+        private BackgroundWorker worker;
 
         public MainWindow()
         {
             InitializeComponent();
+            cancelButton.IsEnabled = false;
+            generateAndStartSorting.IsEnabled = true;
+            resetProgress();
         }        
 
         //Get All the sorter methods selected by User.
@@ -36,61 +44,101 @@ namespace Projeto_Ordenacao_WPF
             return allSelectedMethods;
         }
 
-
-        private void GerarOrdenarNumeros()
+        // Background Worker Methods ===============================
+        private void worker_Start()
         {
-            //Get All methods
-            List<ISort> sorterMethods = getAllSorterMethods();
+            //reset status
+            resetProgress();
 
-            int currentIndexMethod = 0;
-            int totalMethods = sorterMethods.Count;
+            //Enable-Disable Buttons
+            generateAndStartSorting.IsEnabled = false;
+            cancelButton.IsEnabled = true;
+
+            //Get All methods
+            sorterMethods = getAllSorterMethods();
+
+            currentIndexMethod = 0;
+            totalMethods = sorterMethods.Count;
 
             //Instantiate a new list of all sorter methods information
             listOfStatistics = new List<Statistic>();
-            
+
             //Update progress "Generating random Numbers."
-            atualizarProgresso(currentIndexMethod, totalMethods, "Gerando Numeros");
-            SorterSetup.setup(numerosParaGerar);
-            
+            SorterSetup.setup(amountOfNumbersToGenerate);
+
+            //Worker Setup
+            worker = new BackgroundWorker();
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+
+            worker.RunWorkerAsync();
+        }
+        
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
             //Foreach method it will update progress and execute the sorter.
-            foreach(ISort sortMethod in sorterMethods) 
+            foreach (ISort sortMethod in sorterMethods)
             {
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
                 currentIndexMethod++;
-                atualizarProgresso(currentIndexMethod, totalMethods, sortMethod.getMethodName());
+                currentMethodName = sortMethod.getMethodName();
+                worker.ReportProgress(currentIndexMethod);                
                 sortMethod.sort();
                 listOfStatistics.Add(sortMethod.getStatistics());
-            }
-            
-            //Finaliza o método.
-            atualizarProgresso(currentIndexMethod, totalMethods, "null");
+            }                        
         }
 
-        // ================================== BOTOES E COMPONENTES DAS TELAS ===================================//
-        
-        //Update the progress in TextBlock
-        private void atualizarProgresso(int atual, int total, string nomeMetodo)
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            lbl_TitleStatus.Content = $"Ordenando! ({atual}/{total})";
-            barraProgresso.Value = (atual * 100) / total;
-            
-            if (nomeMetodo == null) return;
-            lbl_StatusProgress.Content = $"Método em Execução: {nomeMetodo}"; // Repensar nessa atribuição
+            lbl_TitleStatus.Content = $"Ordenando! ({currentIndexMethod}/{totalMethods})";
+            barraProgresso.Value = (currentIndexMethod * 100) / totalMethods;
+
+            if (currentMethodName == null) return;
+            lbl_StatusProgress.Content = $"Método em Execução: {currentMethodName}";
 
             if (txtBlock_log.Text.Length > 0) //Check if the text box has something.
             {
                 int previousText = txtBlock_log.Text.IndexOf("Em Andamento", 0); //Search for "Em Andamento" text index.
-                txtBlock_log.Text = txtBlock_log.Text.Substring(0, previousText) + "(OK)\n";               
-                
-                if (nomeMetodo.Equals("null"))
-                {
-                    txtBlock_log.Text += "\n===== Ordenação Concluiía!! =====\n\n";
-                    txtBlock_log.Text += $"Amostra original: {numerosParaGerar} numeros desordenados de 0 a 1000\n";
-                    foreach (Statistic info in listOfStatistics) txtBlock_log.Text += info.getStatistic();
-                    return;
-                }
+                txtBlock_log.Text = txtBlock_log.Text.Substring(0, previousText) + "(OK)\n";
             }
-            txtBlock_log.Text += nomeMetodo + "..... \tEm Andamento\n";
+            txtBlock_log.Text += currentMethodName + "..... \tEm Andamento\n";
         }
+
+        void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                //was canceled
+                txtBlock_log.Text += "\n===== Ordenação Cancelada!! =====\n\n";
+                txtBlock_log.Text += $"Amostra original: {amountOfNumbersToGenerate} numeros desordenados de 0 a 1000\n";
+                foreach (Statistic info in listOfStatistics) txtBlock_log.Text += info.getStatistic();
+            }
+            else
+            {
+                //complete
+                txtBlock_log.Text += "\n===== Ordenação Concluiía!! =====\n\n";
+                txtBlock_log.Text += $"Amostra original: {amountOfNumbersToGenerate} numeros desordenados de 0 a 1000\n";
+                foreach (Statistic info in listOfStatistics) txtBlock_log.Text += info.getStatistic();
+            }
+            
+            currentMethodName = null;
+            currentIndexMethod = 0;
+            totalMethods = 0;
+
+            generateAndStartSorting.IsEnabled = true;
+            cancelButton.IsEnabled = false;
+        }
+
+
+        // ================================== BOTOES E COMPONENTES DAS TELAS ===================================//
 
         //Check if the user write letter in txtBox
         private void OnlyNumbers_KeyUp(object sender, TextChangedEventArgs e)
@@ -105,16 +153,23 @@ namespace Projeto_Ordenacao_WPF
         //Start sort process
         private void Button_GerarNumerosOrdenar(object sender, RoutedEventArgs e)
         {
-            int.TryParse(N_ToGenerate.Text, out numerosParaGerar);
-            if (numerosParaGerar <= 0) MessageBox.Show("Digite a quantidade de numeros desejada para ordenar que seja maior que zero!");
-            if (getAllSorterMethods().Count == 0) MessageBox.Show("Selecione ao menos UM metodo de ordenação!!");
-            resetProgress();
-            GerarOrdenarNumeros();
+            int.TryParse(N_ToGenerate.Text, out amountOfNumbersToGenerate);
+            if (amountOfNumbersToGenerate <= 0)
+            {
+                MessageBox.Show("Digite a quantidade de numeros desejada para ordenar que seja maior que zero!");
+                return;
+            }
+            else if (getAllSorterMethods().Count <= 0)
+            {
+                MessageBox.Show("Selecione ao menos UM metodo de ordenação!!");
+                return;
+            }            
+            worker_Start();
         }
 
-        private void Button_ResetProgress(object sender, RoutedEventArgs e)
+        private void Button_Cancel(object sender, RoutedEventArgs e)
         {
-            resetProgress();
+            worker.CancelAsync();
         }
 
         private void resetProgress()
